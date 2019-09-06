7793287ce5524ed13b53a7d151e2edb7eb8b7ee8
github username <github email address>
2019-09-06 12:40:01 -0400

    login implemented

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+class StreamCreate extends React.Component {
+        render() {
+            return <div>StreamCreate</div>
+        }
+    }
+
+export default StreamCreate
+
